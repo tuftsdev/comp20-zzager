bcd7a8c6dfd5487e4363aae7b1d642d69c2b160b
Zach Zager <zachzager@gmail.com>
2016-03-03 18:29:45 -0500

    got the page to display all of the json data

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,28 @@
 // Your JavaScript goes here...
+
+request = new XMLHttpRequest();
+
+function parse() {
+
+	request.open("GET","data.json",true);
+
+	request.onreadystatechange = function() {
+		console.log("Running");
+
+		if (request.readyState == 4 && request.status == 200) {
+			console.log("MING!");
+
+			data = request.responseText;
+			text = JSON.parse(data);
+			theElem = document.getElementById("messages");
+			theElem.innerHTML = "<h4>DATA: </h4>" + request.response;
+		}
+	}
+
+	
+
+	//theElem.innerHTML = JSON.parse(request.responseText);
+
+	request.send(null);
+
+}
