40f2c88f4e426e9d6d59a882216326c5ee4e9207
Zach Zager <zachzager@gmail.com>
2016-03-15 16:28:40 -0400

    put script into separate file, display text on webpage w/ jquery

---
+++ captainslog/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+request = new XMLHttpRequest();
+
+$(document).ready(function(){
+    //theElem = $("#log");
+    $("#log").html("hi there");
+    //theElem. = "hi";
+});
+
+console.log("hello");
