b7d56744ce0e99b414b5b8ec830f7cb8c31a1037
Zach Zager <zachzager@gmail.com>
2016-03-15 16:41:14 -0400

    added new Date() method

--- captainslog/script.js
+++ captainslog/script.js
@@ -1,11 +1,16 @@
 // script.js
 
-request = new XMLHttpRequest();
+// All displayed messages must be preceded with a human readable timestamp 
+// (e.g., "Sat Jul 23 02:16:57 2005", Sun Mar 22 2015 16:05:32 GMT-0400 (EDT)")
+// A space followed by a hyphen followed by a space must follow the timestamp. 
+// Example: Sat Jul 23 02:16:57 2005 - Hello all!
 
 $(document).ready(function(){
-    //theElem = $("#log");
     $("#log").html("hi there");
-    //theElem. = "hi";
 });
 
-console.log("hello");
+var today = new Date();
+
+today += " - Hello all!"
+
+console.log(today);
