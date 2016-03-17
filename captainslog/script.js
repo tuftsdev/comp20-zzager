cccf727d2ceffc38a418b45a801884a6b974ddc1
Zach Zager <zachzager@gmail.com>
2016-03-17 02:03:41 -0400

    script now successfully takes input from local storage and displays it in the "log" section with the most recent logs on top

--- captainslog/script.js
+++ captainslog/script.js
@@ -1,31 +1,23 @@
 // script.js
+// Comp 20 - Lab 7
+// Zach Zager
 
-// All displayed messages must be preceded with a human readable timestamp 
-// (e.g., "Sat Jul 23 02:16:57 2005", Sun Mar 22 2015 16:05:32 GMT-0400 (EDT)")
-// A space followed by a hyphen followed by a space must follow the timestamp. 
-// Example: Sat Jul 23 02:16:57 2005 - Hello all!
-
-var counter = 0;
-//var counter = localStorage.length;
-/*
-for (i = 0; i < counter; i++) {
-
-}
-*/
 $(document).ready(function(){
 
+	// prints the old contents of localStorage
+	for (i = 0; i < localStorage.length; i++) {
+		$("div#log").prepend("<p>"+localStorage[i]+"</p>");
+	}
 
-
-	$("#msg").on("keydown",function(key){
+	$("#msg").on("keydown",function(key){ // key is pressed in the textbox
 		
     	if (key.keyCode == 13) { // Enter (keycode:13) is pressed
-    		today = new Date();
-    		entry = today+" - "+this.value;
-    		localStorage.setItem(counter,entry);
-    		this.value = "";
-    		$("div#log").prepend(localStorage[counter]);
-    		console.log(localStorage[counter]);
-    		counter++;
+    		entry = new Date()+" - "+this.value;
+    		localStorage.setItem(localStorage.length,entry); // sets item in localStorage
+    		this.value = ""; // clears input text box
+
+    		// add most recent post to top of HTML section
+    		$("div#log").prepend("<p>"+localStorage[localStorage.length-1]+"</p>");
     	}
 	});
 
